64b4959b29616f20cfd6b456ec87019e1ea840fa
Ryan Hoffer <ryanlhoffer@gmail.com>
2022-08-04 16:05:46 -0400

    react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="logo" alt="logo"/>
+    </div>
+  );
+}
+
+export default App;
