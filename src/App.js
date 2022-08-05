27cf7a2f389e9fa41d6c5b8d9ed1821bb6745631
Ryan Hoffer <ryanlhoffer@gmail.com>
2022-08-05 08:47:44 -0400

    Added strobing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import logo from './logo.jpg';
+import darklogo from './darklogo.jpg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const images = [logo, darklogo];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  let image;
+  let bg;
+
+  useEffect(() => {
+    if (currentIndex || !currentIndex) {
+        const timeoutID = setTimeout(() => {
+          setCurrentIndex(currentIndex === 0 ? 1 : 0);
+        }, 100);
+        return () => clearTimeout(timeoutID);
+    }
+  });
+  
+  if(currentIndex){
+    image = <img className="logo" alt="logo" src={logo} />
+    bg = 'white'
+  }else{
+    image = <img className="logo invert" alt="logo" src={logo}/>
+    bg = 'black'
+  }
   return (
     <div className="App">
-        <img src={logo} className="logo" alt="logo"/>
+        <div className={bg}>
+          {image}
+        </div>
     </div>
+    
   );
 }
 
